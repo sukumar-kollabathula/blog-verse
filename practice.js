88da47c5e6b3069aac57819f68a26761c791e993
sukumar-kollabathula <sukumar.kollabathula@gmail.com>
2025-09-06 12:35:04 +0530

    practice

--- practice.js
+++ practice.js
@@ -1,1 +1,32 @@
 console.log("hello world")
+let name = 'devastra'
+console.log(name)
+
+let num = 22;
+console.log(num)
+ let isLogged=false;
+ console.log(isLogged)
+
+ let x;
+ console.log(x)
+
+ let y=null;
+    console.log(y)
+
+console .log(typeof name)
+console.log(typeof num)
+console.log(typeof isLogged)
+console.log(typeof x)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+ let person ={
+    name:"sukumar",
+    age:22
+ 
+}
+console.log(person)
+console.log(typeof person)
+console.log(person.name)
+console.log(person.age)
