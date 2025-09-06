23a502d639321ff5e3c64fef65a023c811968508
sukumar-kollabathula <sukumar.kollabathula@gmail.com>
2025-09-06 17:13:07 +0530

    asyncAwait

---
+++ asyncAwait.js
@@ -0,0 +1,11 @@
+const sampledata=async()=>{
+    try{
+        let response=await fetch('https://www.google.com');
+        
+        console.log(response)
+    }
+    catch(err){
+        console.log("error accessing google",err)
+    }
+}
+sampledata();
