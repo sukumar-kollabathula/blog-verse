e3cb8fd454c1df4f131e1d73980a4d246b35af73
sukumar-kollabathula <sukumar.kollabathula@gmail.com>
2025-09-06 15:54:46 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,23 @@
+
+function greet(){
+    console.log("Hello World")
+}
+greet()
+
+const hello = (x) => {
+   return x*x;
+}
+hello(5)
+
+function add(a,b){
+    return a+b;
+}
+let result=add(5,10);
+console.log(result)
+//arrow function
+const square=(x)=>x*x;
+console.log(square(5));
+const subtract=(a,b)=>{
+    return a-b;
+}
+console.log(subtract(10,5));
